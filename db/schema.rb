3f7714878d1fa63960564b685fb9527f7ae2674f
PhilippeJones <philippejones@gmail.com>
2016-06-04 20:38:54 -0400

    Changed to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160604021444) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "entries", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
